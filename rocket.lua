1b5367f532598f029000a70997a9893a42c6e477
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-04-10 19:12:11 +0200

    move rocket to separate file

---
+++ rocket.lua
@@ -0,0 +1,37 @@
+
+minetest.register_craftitem("glider:rocket", {
+	description = "Rocket (Use while gliding to boost delta glider speed)",
+	inventory_image = "glider_rocket.png",
+	on_use = function(itemstack, user, pt) --luacheck: no unused args
+		local attach = user:get_attach()
+		if attach then
+			local luaent = attach:get_luaentity()
+			if luaent.name == "glider:hangglider" then
+				luaent.speed = luaent.speed + luaent.time_from_last_rocket
+				luaent.time_from_last_rocket = 0
+				itemstack:take_item()
+				minetest.add_particlespawner({
+					amount = 1000,
+					time = 2,
+					minpos = { x = -0.125, y = -0.125, z = -0.125 },
+					maxpos = { x = 0.125, y = 0.125, z = 0.125 },
+					minexptime = 0.5,
+					maxexptime = 1.5,
+					attached = attach,
+					texture = "glider_rocket_particle.png",
+				})
+				return itemstack
+			end
+		end
+	end
+})
+
+minetest.register_craft({
+	output = "glider:rocket 33",
+	recipe = {
+		{ "group:wood", "tnt:gunpowder", "group:wood" },
+		{ "group:wood", "tnt:gunpowder", "group:wood" },
+		{ "group:wood", "tnt:gunpowder", "group:wood" },
+	}
+})
+
