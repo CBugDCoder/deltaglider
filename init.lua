e1dd174555b3f924689d8a5537d41f890e523e49
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-04-10 12:41:57 +0200

    whitespace cleanup

--- init.lua
+++ init.lua
@@ -2,21 +2,22 @@ local function rot_to_dir(rot)
 	local x = -math.cos(rot.x) * math.sin(rot.y)
 	local y = math.sin(rot.x)
 	local z = math.cos(rot.x) * math.cos(rot.y)
-	return {x = x, y = y, z = z}
+	return { x = x, y = y, z = z }
 end
 
 local function get_pitch_lift(y)
-	local l = -(1964/1755*y*y*y*y)-(2549/3510*y*y*y)+(2591/7020*y*y)+(2594/3510*y)+(.75)
+	local l = -(1964 / 1755 * y * y * y * y) - (2549 / 3510 * y * y * y)
+		+ (2591 / 7020 * y * y) + (2594 / 3510 * y) + .75
 	return l
 end
 
 local mouse_controls = minetest.settings:get_bool("glider_mouse_controls", true)
 
 local on_step = function(self, dtime, moveresult)
-	self.time_from_last_rocket = math.min(self.time_from_last_rocket+dtime,10)
+	self.time_from_last_rocket = math.min(self.time_from_last_rocket+dtime, 10)
 	local vel = self.object:get_velocity()
 	local speed = self.speed
-	--local actual_speed = math.sqrt(vel.x^2+vel.y^2+vel.z^2)
+	--local actual_speed = math.sqrt(vel.x ^ 2 + vel.y ^ 2 + vel.z ^ 2)
 	local rot = self.object:get_rotation()
 	local driver = minetest.get_player_by_name(self.driver)
 	local pos = self.object:get_pos()
@@ -25,38 +26,38 @@ local on_step = function(self, dtime, moveresult)
 	local land = false
 	local crash_speed = 0
 	if moveresult and moveresult.collisions and moveresult.collides then
-		for _,collision in pairs(moveresult.collisions) do
+		for _ ,collision in pairs(moveresult.collisions) do
 			land = true
-			crash_speed = crash_speed+
-						  math.abs(collision.old_velocity.x-collision.new_velocity.x)+
-						  math.abs(collision.old_velocity.y-collision.new_velocity.y)+
-						  math.abs(collision.old_velocity.z-collision.new_velocity.z)
+			crash_speed = crash_speed
+				+ math.abs(collision.old_velocity.x - collision.new_velocity.x)
+				+ math.abs(collision.old_velocity.y - collision.new_velocity.y)
+				+ math.abs(collision.old_velocity.z - collision.new_velocity.z)
 		end
 	end
-	
+
 	if land then
 		driver:set_detach()
-		driver:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
+		driver:set_eye_offset({ x = 0, y = 0, z = 0 }, { x = 0, y = 0, z = 0})
 		driver:add_player_velocity(vel)
-		local crash_dammage = math.floor(math.max(crash_speed-5, 0))
+		local crash_dammage = math.floor(math.max(crash_speed - 5, 0))
 		if crash_dammage > 0 then
 			local node = minetest.get_node(pos)
 			if minetest.registered_nodes[node.name].liquidtype == "none" then
 				local hp = driver:get_hp()
-				driver:set_hp(hp-crash_dammage, {type = "fall"})
+				driver:set_hp(hp - crash_dammage, { type = "fall" })
 			end
 		end
 		self.object:remove()
 	end
-	
+
 	if mouse_controls then
-		rot.x = rot.x + (-driver:get_look_vertical()-rot.x)*(dtime*2)
+		rot.x = rot.x + (-driver:get_look_vertical() - rot.x) * dtime * 2
 		local hor = driver:get_look_horizontal()
-		local angle = hor-rot.y
-		if angle < -math.pi then angle = angle + math.pi*2 end
-		if angle > math.pi then angle = angle - math.pi*2 end
-		rot.y = rot.y + angle*(dtime*2)
-		speed = speed - math.abs(angle*dtime)
+		local angle = hor - rot.y
+		if angle < -math.pi then angle = angle + math.pi * 2 end
+		if angle > math.pi then angle = angle - math.pi * 2 end
+		rot.y = rot.y + angle * dtime * 2
+		speed = speed - math.abs(angle * dtime)
 		rot.z = -angle
 	else
 		local control = driver:get_player_control()
@@ -67,29 +68,29 @@ local on_step = function(self, dtime, moveresult)
 			rot.x = rot.x - dtime
 		end
 		if control.left then
-			rot.z = rot.z - 2*dtime
+			rot.z = rot.z - 2 * dtime
 		end
 		if control.right then
-			rot.z = rot.z + 2*dtime
+			rot.z = rot.z + 2 * dtime
 		end
-		
+
 		if rot.z ~= 0 then
-			speed = speed - math.abs(rot.z*dtime)
+			speed = speed - math.abs(rot.z * dtime)
 			if math.abs(rot.z) < 0.01 then
 				rot.z = 0
 			end
-			rot.y = rot.y - (rot.z*dtime)
-			rot.z = rot.z - rot.z*dtime
+			rot.y = rot.y - rot.z * dtime
+			rot.z = rot.z - rot.z * dtime
 		end
 	end
-	
-	speed = math.min(math.max((speed - (rot.x^3)*4 * dtime) - speed * 0.01 * dtime, 2),30)
+
+	speed = math.min(math.max((speed - (rot.x ^ 3) * 4 * dtime) - speed * 0.01 * dtime, 2), 30)
 	self.object:set_rotation(rot)
 	local dir = rot_to_dir(rot)
-	local lift = (speed/2) * get_pitch_lift(dir.y) * (1-(math.abs(rot.z/math.pi)))
-	local vertical_acc = lift-5
-	self.grav_speed = math.min(math.max(self.grav_speed + vertical_acc*dtime,-10),1)
-	dir = {x = dir.x*speed, y = dir.y*speed+self.grav_speed, z = dir.z*speed}
+	local lift = (speed * 0.5) * get_pitch_lift(dir.y) * (1 - (math.abs(rot.z / math.pi)))
+	local vertical_acc = lift - 5
+	self.grav_speed = math.min(math.max(self.grav_speed + vertical_acc * dtime, -10), 1)
+	dir = { x = dir.x * speed, y = dir.y * speed + self.grav_speed, z = dir.z * speed }
 	self.speed = speed
 	self.object:set_velocity(dir)
 end
@@ -102,7 +103,7 @@ minetest.register_entity("glider:hangglider", {
 	pointable = false,
 	visual = "mesh",
 	mesh = "glider_hangglider.obj",
-	textures = {"glider_hangglider.png"},
+	textures = { "glider_hangglider.png" },
 	static_save = false,
 	--Functions
 	on_step = on_step,
@@ -121,13 +122,13 @@ minetest.register_tool("glider:glider", {
 		local pos = user:get_pos()
 		local attach = user:get_attach()
 		local luaent
-		if attach then 
+		if attach then
 			luaent = attach:get_luaentity()
 			if luaent.name == "glider:hangglider" then
 				local vel = attach:get_velocity()
 				attach:remove()
 				user:set_detach()
-				user:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
+				user:set_eye_offset({ x = 0, y = 0, z = 0}, { x = 0, y = 0, z = 0 })
 				user:add_player_velocity(vel)
 			end
 		else
@@ -135,13 +136,13 @@ minetest.register_tool("glider:glider", {
 			local ent = minetest.add_entity(pos, "glider:hangglider")
 			luaent = ent:get_luaentity()
 			luaent.driver = name
-			local rot = {y = user:get_look_horizontal(), x = -user:get_look_vertical(), z = 0}
+			local rot = { y = user:get_look_horizontal(), x = -user:get_look_vertical(), z = 0 }
 			ent:set_rotation(rot)
-			local vel = vector.multiply(user:get_player_velocity(),2)
+			local vel = vector.multiply(user:get_player_velocity(), 2)
 			ent:set_velocity(vel)
-			luaent.speed = math.sqrt(vel.x^2+(vel.y/4)^2+vel.z^2)
-			user:set_attach(ent, "", {x=0,y=0,z=-10}, {x=90,y=0,z=0})
-			user:set_eye_offset({x=0,y=-16.25,z=0},{x=0,y=-15,z=0})
+			luaent.speed = math.sqrt(vel.x ^ 2 + (vel.y * 0.25) ^ 2 + vel.z ^ 2)
+			user:set_attach(ent, "", { x = 0, y = 0, z = -10 }, { x = 90, y = 0, z = 0 })
+			user:set_eye_offset({ x = 0, y = -16.25, z = 0 },{ x = 0, y = -15, z = 0 })
 		end
 	end,
 })
@@ -160,8 +161,8 @@ minetest.register_craftitem("glider:rocket", {
 				minetest.add_particlespawner({
 					amount = 1000,
 					time = 2,
-					minpos = {x = -0.125, y = -0.125, z = -0.125},
-					maxpos = {x = 0.125, y = 0.125, z = 0.125},
+					minpos = { x = -0.125, y = -0.125, z = -0.125 },
+					maxpos = { x = 0.125, y = 0.125, z = 0.125 },
 					minexptime = 0.5,
 					maxexptime = 1.5,
 					attached = attach,
@@ -176,17 +177,17 @@ minetest.register_craftitem("glider:rocket", {
 minetest.register_craft({
 	output = "glider:glider",
 	recipe = {
-		{"group:wool", "group:wool", "group:wool" },
-		{"group:stick","",           "group:stick"},
-		{"",           "group:stick",""           },
+		{ "group:wool", "group:wool", "group:wool" },
+		{ "group:stick", "", "group:stick" },
+		{ "", "group:stick", "" },
 	}
 })
 
 minetest.register_craft({
 	output = "glider:rocket 33",
 	recipe = {
-		{"group:wood","tnt:gunpowder","group:wood"},
-		{"group:wood","tnt:gunpowder","group:wood"},
-		{"group:wood","tnt:gunpowder","group:wood"},
+		{ "group:wood", "tnt:gunpowder", "group:wood" },
+		{ "group:wood", "tnt:gunpowder", "group:wood" },
+		{ "group:wood", "tnt:gunpowder", "group:wood" },
 	}
 })
