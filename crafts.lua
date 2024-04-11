85640e3fa83b3cdb656d629e41db582f0980d764
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-04-11 06:12:35 +0200

    preserve colour in repair recipes

--- crafts.lua
+++ crafts.lua
@@ -40,6 +40,16 @@ local function get_color_name(name)
 	return name
 end
 
+local function get_color_name_from_color(color)
+	for name, color_hex in pairs(dye_colors) do
+		if color == color_hex then
+			return name
+		end
+	end
+
+	return nil
+end
+
 -- This recipe is just a placeholder
 do
 	local item = ItemStack("glider:glider")
@@ -61,15 +71,22 @@ minetest.register_on_craft(function(crafted_item, _, old_craft_grid)
 		local name = stack:get_name()
 		if name == "glider:glider" then
 			wear = stack:get_wear()
+			color = stack:get_meta():get("hangglider_color")
+			color_name = get_color_name_from_color(color)
 		elseif minetest.get_item_group(name, "dye") ~= 0 then
 			color = get_dye_color(name)
 			color_name = get_color_name(name)
+		elseif "wool:white" == stack:get_name()
+			or "default:paper" == stack:get_name()
+		then
+			wear = 0
 		end
 	end
 	if wear and color and color_name then
 		if color == "ffffff" then
 			return ItemStack({ name = "glider:glider", wear = wear })
 		end
+
 		local meta = crafted_item:get_meta()
 		meta:set_string("description", color_name .. " Delta Glider")
 		meta:set_string("inventory_image",
