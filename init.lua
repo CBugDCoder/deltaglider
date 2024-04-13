e7581da89eba5788ccd776a1616ad9f670398c0b
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-04-13 22:35:32 +0200

    version bump 20240413.22340

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ local vector_zero = vector.zero
 
 -- global table for exposed functions
 glider = {
-	version = 20240411.182102,
+	version = 20240413.223400,
 }
 
 local has_areas = minetest.get_modpath("areas")
